75425dd8fc9d937fe45110de6100c79c8e6534a0
Alexey Polyakov <folsten@alfa.im>
2023-11-28 22:39:23 +0300

    basic project config done and layouting started

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,27 @@ module.exports = {
   content: ["./src/**/*.{html,js,php}"],
   darkMode: "class",
   theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
+      'xs': {'max': '399px'},
+    },
+    container: {
+      center: true
+    },
+    colors: {
+      white: 'white',
+      black: 'black',
+      text: '#353535',
+      'green-100': '#45D354',
+      'green-200': '#2EBC3D',
+      'blue-100': '#37AEE2',
+      'blue-200': '#1F96CA',
+      orange: 'linear-gradient(92.6deg, #FA8116 -32.07%, #FFCA2B 104.28%)',
+    },
     extend: {},
   },
   plugins: plugins,
