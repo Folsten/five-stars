7f361801f3adc477982d4fa2d62eb1f0f158ce16
Alexey Polyakov <folsten@alfa.im>
2023-11-29 19:44:53 +0300

    slider done

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
 window.addEventListener("DOMContentLoaded", () => {
   console.log("Loaded Scripts");
 });
+
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-main-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    prevEl: '.swiper-button-left',
+    nextEl: '.swiper-button-right',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
