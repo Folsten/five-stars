5ddacbc8a2114afbea49208c06ca9779abb98bbc
Alexey Polyakov <folsten@alfa.im>
2023-11-30 10:01:11 +0300

    progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,34 @@
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("Loaded Scripts");
+    console.log("Loaded Scripts");
+    document.querySelectorAll(".buttonHollow-orange").forEach(function (item) {
+        item.addEventListener('mouseover', function () {
+            item.querySelector('.buttonHollow-orange__phone-orange').style.display = 'none';
+            item.querySelector('.buttonHollow-orange__phone-white').style.display = 'flex';
+        })
+    })
+    document.querySelectorAll(".buttonHollow-orange").forEach(function (item) {
+        item.addEventListener('mouseleave', function () {
+            item.querySelector('.buttonHollow-orange__phone-orange').style.display = 'flex';
+            item.querySelector('.buttonHollow-orange__phone-white').style.display = 'none';
+        })
+    })
 });
 
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-main-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    prevEl: '.swiper-button-left',
-    nextEl: '.swiper-button-right',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-
-// additional config
-  spaceBetween: 70,
-  autoplay: {
-    delay: 2000
-  },
+    direction: 'horizontal',
+    loop: true, // зацикленность слайдов, должны бить или нет, значения true / false
+    pagination: {
+        el: '.swiper-main-pagination',
+        clickable: true,
+    },
+    navigation: {
+        prevEl: '.swiper-button-left',
+        nextEl: '.swiper-button-right',
+    },
+    spaceBetween: 70, // регулировка расстояния между слайдами
+    // autoplay: {
+    //   delay: 2000 // задержка при авто-пролистывании
+    // },
 });
